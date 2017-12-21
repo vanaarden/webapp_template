e4db7d0b130e346d199058a0afa1a7e7b663ce40
Rens <rens@vanaarden.com>
2017-12-21 10:32:06 +0100

    Added dist

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            minify: {
+                src: 'reset.css',
+                dest: 'style-min.css'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            build: {
+                src: '*.js',
+                dest: 'build/js/<%= pkg.name %>.min.js'
+            }
+        },
+
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: {
+                    'dist/app.js': 'script.js'
+                }
+            }
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.registerTask('default', ['babel']);
+
+
+};
